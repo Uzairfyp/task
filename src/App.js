05ffe23db9f38c388b4b2a61e7e0a79c280e23bd
uzair <uzairrehman143@gmail.com>
2025-10-23 01:56:48 +0500

    Setup package.json and prepare for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom"; // Add this
+import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
-import Footer from "./components/Footer";
 import AIPhotoSection from "./components/AIPhotoSection";
 import PhotoGallerySection from "./components/PhotoGallerySection";
 import TestimonialsSection from "./components/TestimonialsSection";
@@ -10,22 +9,21 @@ import EventCreationFlow from "./components/EventCreationFlow";
 import PricingPlans from "./components/PricingPlans";
 import EnterprisePlanCard from "./components/EnterprisePlanCard";
 import SmarterPhotoManagement from "./components/SmarterPhotoManagement";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Router basename="/task"> {/* Set the basename to your repo name */}
+    <Router basename="/task"> {/* Required for GitHub Pages subpath */}
       <div className="font-sans">
         <Navbar />
         <HeroSection />
         <AIPhotoSection />
-        {/* //<FeaturesSection /> */}
-        {/* <CallToActionSection /> */}
-        <PhotoGallerySection/>
-        <TestimonialsSection/>
-        <EventCreationFlow/>
-        <PricingPlans/>
+        <PhotoGallerySection />
+        <TestimonialsSection />
+        <EventCreationFlow />
+        <PricingPlans />
         <EnterprisePlanCard />
-        <SmarterPhotoManagement/>
+        <SmarterPhotoManagement />
         <Footer />
       </div>
     </Router>
